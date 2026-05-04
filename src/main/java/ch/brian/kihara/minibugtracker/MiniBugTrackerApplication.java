bf12cb5e9cb4b4ace9ca42aa3e1dc5591a724ff8
admin.local <kiharacollins@gmail.com>
2026-05-04 15:59:01 +0200

    init push

---
+++ src/main/java/ch/brian/kihara/minibugtracker/MiniBugTrackerApplication.java
@@ -0,0 +1,13 @@
+package ch.brian.kihara.minibugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBugTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniBugTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/brian/kihara/minibugtracker/MiniBugTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.brian.kihara.minibugtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBugTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
